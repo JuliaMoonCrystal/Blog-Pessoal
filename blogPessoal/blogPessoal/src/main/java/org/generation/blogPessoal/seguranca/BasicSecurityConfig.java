3549744fb3e0e5eb278caf4a38b02101cfeba562
julia <jufarias20002@gmail.com>
2021-10-14 12:29:27 -0300

    Preparando para subir a API na nuvem

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package org.generation.blogPessoal.seguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
    
@@ -19,7 +22,14 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
+		
+        auth.userDetailsService(userDetailsService);
+		
+		auth.inMemoryAuthentication()
+		.withUser("root")
+		.password(passworldEncolder().encode("root"))
+		.authorities("ROLE_USER");
+	
 	}
 	
 	@Bean
@@ -29,14 +39,17 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/usuarios/logar").permitAll()
+	protected  void  configure(HttpSecurity  http) throws  Exception {
+
+	     http.authorizeRequests()
 		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	    .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().cors()
 		.and().csrf().disable();
 	}
+
 }
