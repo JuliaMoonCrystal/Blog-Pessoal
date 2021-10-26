128fb1d46db920e9559ba04adfd46d602b8b2a57
julia <jufarias20002@gmail.com>
2021-10-26 16:52:15 -0300

    teste com o gitignore

--- blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/App.tsx
+++ blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/App.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import Home from './paginas/home/Home';
 /*import logo from './logo.svg';*/
 import './App.css';
+import { Grid } from '@material-ui/core';
 
 /*let nome ='Jujuba'*/
 function App() {
   return (
-   /* <div className="App">
+    <>
+    
+    </>
+  );
+}
+
+export default App;
+
+/*
+Aula 10
+
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,9 +34,40 @@ function App() {
         </a>
       </header>
     </div>
-    <h1>{nome}</h1>*/
-    <Home />
-  );
-}
+    <h1>{nome}</h1>
 
-export default App;
+
+    <Home />
+
+    Aula 11
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={12}>
+      <Home />
+      </Grid>
+
+      <Grid item xs={12} sm={6}>
+      <Home />
+      </Grid>
+
+      <Grid item xs={12} sm={6}>
+      <Home />
+      </Grid>
+
+      <Grid item xs={12} sm={3}>
+      <Home />
+      </Grid>
+
+      <Grid item xs={12} sm={3}>
+      <Home />
+      </Grid>
+
+      <Grid item xs={12} sm={3}>
+      <Home />
+      </Grid>
+
+      <Grid item xs={12} sm={3}>
+      <Home />
+      </Grid>
+      
+    </Grid>
+    */
--- blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/paginas/home/Home.tsx
+++ blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/paginas/home/Home.tsx
@@ -1,14 +1,53 @@
 import React from "react";
+import { Paper, Box,Button } from "@material-ui/core";
 import './Home.css';
 
 function Home() {
     return(
         <div>
-            <h1 className='titulo'>Fairy Tail</h1>
-         <img className='imagem' src='https://i0.wp.com/img4.goodfon.com/wallpaper/nbig/d/a2/fairy-tail-natsu-dragneel-dragon-dragon-slayer-anime-manga-g.jpg' alt="tela inicial"></img>
+           <Paper>
+               <Box p={2}>
+                   <Box display='flex' justifyContent='center'>
+                       <h1>Fairy Tail</h1>
+                   </Box>
+                   <img src='https://ptanime.com/wp-content/uploads/2015/02/Fairy-Tail-411-Gray-e-Natsu-Sobrevivem.jpg' alt='fairy' height='100%' width='100%'></img>
+                   <Box display='flex' justifyContent='center' p={2}>
+                       <Button variant='contained' color='primary' >Fairy 1</Button>
+                       
+                       <Button variant='contained' color='secondary'>Fairy 2</Button>
+                           </Box>
+               </Box>
+           </Paper>
         </div>
     );
 
 }
 
 export default Home;
+
+/* <div>
+Aula 10
+           <Grid container spacing={2}> 
+               <Grid item xs={12} sm={8} >
+               <h1>Grid 1 --  parte do Grid principal</h1>
+                  <Paper style={{height: '100vh', background: 'red'}}/>
+            
+                 
+               </Grid >
+
+               <Grid item container direction='column' xs={12} sm={4} spacing={3}>
+                   <h1>Grid 2 --  parte do Grid principal</h1>
+                   <Grid item>
+                     <Paper style={{height: '49vh', background: 'blue'}}/>
+                      
+                   </Grid>
+           
+                   <Grid item>
+                    <Paper style={{height: '49vh', background: 'gold'}}/>
+                      
+                     
+                   </Grid>
+
+               </Grid >    
+           </Grid>
+        </div>*/
