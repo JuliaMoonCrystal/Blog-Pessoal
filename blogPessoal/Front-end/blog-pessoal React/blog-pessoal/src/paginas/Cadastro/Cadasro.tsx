8a73abd41b47f8b64c707ed0c411c924842ccfc3
julia <jufarias20002@gmail.com>
2021-11-02 20:24:33 -0300

    Criando a pasta e arquivos de cadastro

---
+++ blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/paginas/Cadastro/Cadasro.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cadastro() {
+    return(
+       <>
+       </>
+    );
+
+}
+
+export default Cadastro;
--- blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/paginas/home/Home.tsx
+++ blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/paginas/home/Home.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Paper, Box,Button, Grid, Typography } from "@material-ui/core";
+import { Paper, Box, Button, Grid, Typography } from "@material-ui/core";
 import './Home.css';
 
 function Home() {
-    return(
+    return (
         <div>
 
-           <Grid container direction="row" justifyContent="center" alignItems="center" className='fundo-1'>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='fundo-1'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={0} >
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "Gold", fontWeight: "bold" }}>Seja bem vinde!</Typography>
@@ -43,7 +43,7 @@ function Home() {
                 <Grid xs={12} style={{ backgroundColor: "white" }}>
                 </Grid>
             </Grid>
-         
+
         </div>
     );
 
@@ -53,31 +53,31 @@ export default Home;
 
 /* <div>
 Aula 10
-           <Grid container spacing={2}> 
+           <Grid container spacing={2}>
                <Grid item xs={12} sm={8} >
                <h1>Grid 1 --  parte do Grid principal</h1>
                   <Paper style={{height: '100vh', background: 'red'}}/>
-            
-                 
+
+
                </Grid >
 
                <Grid item container direction='column' xs={12} sm={4} spacing={3}>
                    <h1>Grid 2 --  parte do Grid principal</h1>
                    <Grid item>
                      <Paper style={{height: '49vh', background: 'blue'}}/>
-                      
-                   </Grid>
-           
-                   <Grid item>
-                    <Paper style={{height: '49vh', background: 'gold'}}/>
-                      
-                     
+
                    </Grid>
 
-               </Grid >    
+                   <Grid item>
+                    <Paper style={{height: '49vh', background: 'gold'}}/>
+
+
+                   </Grid>
+
+               </Grid >
            </Grid>
         </div>
-        
+
          <Paper>
                <Box p={2}>
                    <Box display='flex' justifyContent='center'>
@@ -86,10 +86,10 @@ Aula 10
                    <img src='https://ptanime.com/wp-content/uploads/2015/02/Fairy-Tail-411-Gray-e-Natsu-Sobrevivem.jpg' alt='fairy' height='100%' width='100%'></img>
                    <Box display='flex' justifyContent='center' p={2}>
                        <Button variant='contained' color='primary' >Fairy 1</Button>
-                       
+
                        <Button variant='contained' color='secondary'>Fairy 2</Button>
                            </Box>
                </Box>
            </Paper>
-        
+
         */
