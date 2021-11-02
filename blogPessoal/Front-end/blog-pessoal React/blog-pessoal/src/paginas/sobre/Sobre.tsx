a7df2d7e6610e913f4c7aa0731759f6daa1d15d2
julia <jufarias20002@gmail.com>
2021-11-02 16:21:17 -0300

    estruturando o sobre

--- blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/paginas/sobre/Sobre.tsx
+++ blogPessoal/Front-end/blog-pessoal React/blog-pessoal/src/paginas/sobre/Sobre.tsx
@@ -8,18 +8,20 @@ import './Sobre.css';
        <>
          <Grid container direction="row" justifyContent="center" alignItems="center" className='fundo'>
                 <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography className='titulo'>Seja bem vinde!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
-                    </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                        </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Fazer Postagem</Button>
+                    <Box paddingX={10} >
+                        <Typography className='titulo'>Sobre o meu Blog-Pessoal</Typography>
+                        <Typography className='texto'>Expresse aqui os seus pensamentos e opiniões!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto praesentium 
+                        vero sapiente placeat eaque incidunt vitae cupiditate perspiciatis sed officiis modi optio
+                        labore recusandae, provident temporibus pariatur in repellat minima?
+                        
+                        
+                        
+                        </Typography>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img className='legoshi' src="https://i.pinimg.com/originals/cc/8d/21/cc8d2169bb5f02d12e749ef08d4868d9.gif" alt="imagem da home" width="500px" height="500px" />
+                    <img className='sobre' src="http://photo.princesapop.com/br/1/190/moy/151957.jpg" alt="imagem do sobre" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} style={{ backgroundColor: "white" }}>
                 </Grid>
